e5b5e47a7817ba70894615331cacda527f9c33bd
Oleg Smedyuk <conformist.mw@gmail.com>
2022-02-05 17:28:56 +0200

    upgrade django version and dependencies

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,10 @@
 from datacenter.active_passcards_view import active_passcards_view
 from datacenter.passcard_info_view import passcard_info_view
 from datacenter.storage_information_view import storage_information_view
-from django.conf.urls import url
-
+from django.urls import path
 
 urlpatterns = [
-    url(r'^$', active_passcards_view, name="active_passcards"),
-    url(r'^storage_information$', storage_information_view, name="storage_information"),
-    url(r'^passcard_info/(?P<passcode>[\w\-]+)/$', passcard_info_view, name="passcard_info"),
+    path('', active_passcards_view, name='active_passcards'),
+    path('storage_information', storage_information_view, name='storage_information'),
+    path('passcard_info/<uuid:passcode>', passcard_info_view, name='passcard_info'),
 ]
