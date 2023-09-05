74ebd6214481da6f8ee491bcd29b53109dec6de5
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2023-09-05 12:25:17 +0000

    Количество запросов к БД не зависит от количества записей в выборке

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -4,13 +4,11 @@ from django.shortcuts import render
 
 from datacenter.functions import is_visit_long, get_format_entered_at, get_format_duration
 
-
 def storage_information_view(request):
     non_closed_visits = []
-    passcard = Passcard.objects.all()
-    for passcard_value in passcard:
-        visit = Visit.objects.filter(passcard=passcard_value, leaved_at=None)
-        for visit_value in visit:
+
+    visit = Visit.objects.filter(leaved_at=None)
+    for visit_value in visit:
             dictionary = {
             'who_entered': visit_value.passcard.owner_name,
             'entered_at': get_format_entered_at(visit_value),
