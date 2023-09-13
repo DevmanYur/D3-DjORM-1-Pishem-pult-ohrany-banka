3cb37c0aea9f6f6c3869bf4219b22c0371c2b425
DevmanYur <devmanyur@gmail.com>
2023-09-13 20:03:36 +0300

    код запускается командой python manage.py runserver 0.0.0.0:8000

--- main.py
+++
@@ -1,6 +0,0 @@
-import os
-
-from django.core.management import execute_from_command_line
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
